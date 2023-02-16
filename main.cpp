697d700f5374ba0b3602fcded84b15e664cf58d9
Yashvi <ysiddhapura@mail.csuchico.edu>
2023-02-16 10:18:23 -0800

    Added even/odd for loop

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int main() 
 {
     cout << "What's Up?\n";
+
+    for(int i = 0; i<30; i++   )   {
+        if  ( i%2 == 0)  {
+            cout << "EVEN"<<endl;
+
+            }
+        else {
+             cout<<"ODD"<<endl;
+
+            }
+    }
     
     return 0;
 }
